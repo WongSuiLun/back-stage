c8020ec3c63e58d55a67334782531c1277c36ce5
zhp <354416479@qq.com>
2019-05-08 10:09:43 +0800

    add zh setting

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <a-locale-provider :locale="locale">
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  </a-locale-provider>
 </template>
 
 <script>
-import { AppDeviceEnquire } from '@/utils/mixin'
+import zhCN from "ant-design-vue/lib/locale-provider/zh_CN";
+import { AppDeviceEnquire } from "@/utils/mixin";
 export default {
-  name: 'app',
+  name: "app",
   mixins: [AppDeviceEnquire],
-}
+  data() {
+    return {
+      locale: zhCN
+    };
+  }
+};
 </script>
-<style>
-</style>
