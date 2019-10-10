b247c5a951a16266b1e15d439838299cf8998120
zhp <354416479@qq.com>
2019-10-10 12:35:13 +0800

    delete redundant

--- src/views/tags/tagEdit/TagEdit.vue
+++ src/views/tags/tagEdit/TagEdit.vue
@@ -10,16 +10,6 @@
 
     >
     <a-icon slot="suffixIcon" type="meh" />
-      <a-select-opt-group>
-        <span slot="label">
-          <a-icon type="user" />Manager
-        </span>
-        <a-select-option value="jack">Jack</a-select-option>
-        <a-select-option value="lucy">Lucy</a-select-option>
-      </a-select-opt-group>
-      <a-select-opt-group label="Engineer">
-        <a-select-option value="Yiminghe">yiminghe</a-select-option>
-      </a-select-opt-group>
       <a-select-option v-for="i in tagOptions" :key="i.id.toString()">{{i.title}}</a-select-option>
     </a-select>
     <a-divider></a-divider>
